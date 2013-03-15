25c84f585afba98d0e6c884622ff85628dbcbd58
Nabellaleen <ownerfrance+github@hotmail.com>
2013-03-15 22:35:39 +0100

    Add sharebox to content pages

--- content-page.php
+++ content-page.php
@@ -1,4 +1,3 @@
-
 <article id='article-full' class="post-<?php the_ID(); ?>" <?php post_class(); ?> >
 
   <!-- / article title -->
@@ -15,6 +14,18 @@
   <?php echo get_the_post_thumbnail($post->ID, array(450,450), array('class'=>'right last')); ?>
 
   <?php the_content(); ?>
+
+  <div id='sharebox'>
+    <div class='addthis_toolbox addthis_default_style '>
+      <a class='addthis_button_facebook_like' fb:like:layout='button_count'></a>
+	  <a class='addthis_button_tweet' 
+		tw:related='<?php echo revenudebase_twitter_username(); ?>' 
+		tw:via='<?php echo revenudebase_twitter_username(); ?>'></a>
+      <a class='addthis_button_google_plusone' g:plusone:size='medium'></a>
+      <!-- /%a{ :class=>"addthis_counter addthis_pill_style" } -->
+      <script src='http://s7.addthis.com/js/250/addthis_widget.js' type='text/javascript'></script>
+    </div>
+  </div>
 
   <?php comments_template(); ?>
 </article>
